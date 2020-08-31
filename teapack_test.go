4baf865ec25a1a122b62a6fc8084aa826ee233f1
YamiOdymel <yamiodymel@gmail.com>
2020-09-01 04:57:46 +0800

    Test

--- teapack_test.go
+++ teapack_test.go
@@ -1,11 +1,9 @@
 package teapack
 
 import (
-	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/vmihailenco/msgpack"
 )
 
 func TestPacketRequest(t *testing.T) {
@@ -33,12 +31,17 @@ func TestPacketRequest(t *testing.T) {
 	a.NoError(err)
 	a.Equal("world", data["hello"])
 	a.Equal("bar", ctx["foo"])
+	a.Equal(uint16(12345), ID(req))
+	a.Equal(uint8(12), Method(p))
 }
 
-func BenchmarkTeaPackUnmarshal(b *testing.B) {
-	req := &PacketRequest{
+func TestPacketEvent(t *testing.T) {
+	var data map[string]string
+	var ctx map[string]string
+	a := assert.New(t)
+
+	req := &PacketEvent{
 		Method: 12,
-		ID:     12345,
 		Context: map[string]string{
 			"foo": "bar",
 		},
@@ -46,40 +49,27 @@ func BenchmarkTeaPackUnmarshal(b *testing.B) {
 			"hello": "world",
 		},
 	}
-	bin, err := Marshal(req)
-	if err != nil {
-		panic(err)
-	}
-
-	for i := 0; i < b.N; i++ {
-		var data map[string]string
-		p, err := Load(bin)
-		if err != nil {
-			panic(err)
-		}
-		Method(p)
-		if err != nil {
-			panic(err)
-		}
-		ID(p)
-		if err != nil {
-			panic(err)
-		}
-		err = UnmarshalContext(p, &data)
-		if err != nil {
-			panic(err)
-		}
-		err = Unmarshal(p, &data)
-		if err != nil {
-			panic(err)
-		}
-	}
+	b, err := Marshal(req)
+	a.NoError(err)
+	p, err := Load(b)
+	a.NoError(err)
+	err = Unmarshal(p, &data)
+	a.NoError(err)
+	err = UnmarshalContext(p, &ctx)
+	a.NoError(err)
+	a.Equal("world", data["hello"])
+	a.Equal("bar", ctx["foo"])
+	a.Equal(uint8(12), Method(p))
 }
 
-func BenchmarkTeaPackUnmarshalData(b *testing.B) {
-	req := &PacketRequest{
-		Method: 12,
-		ID:     12345,
+func TestPacketResponse(t *testing.T) {
+	var data map[string]string
+	var ctx map[string]string
+	a := assert.New(t)
+
+	req := &PacketResponse{
+		StatusCode: StatusCodeOK,
+		ID:         12345,
 		Context: map[string]string{
 			"foo": "bar",
 		},
@@ -87,127 +77,16 @@ func BenchmarkTeaPackUnmarshalData(b *testing.B) {
 			"hello": "world",
 		},
 	}
-	bin, err := Marshal(req)
-	if err != nil {
-		panic(err)
-	}
-
-	for i := 0; i < b.N; i++ {
-		var data map[string]string
-		p, err := Load(bin)
-		if err != nil {
-			panic(err)
-		}
-		err = Unmarshal(p, &data)
-		if err != nil {
-			panic(err)
-		}
-	}
-}
-
-func BenchmarkJSONUnmarshal(b *testing.B) {
-	req := &PacketRequest{
-		Method: 12,
-		ID:     12345,
-		Context: map[string]string{
-			"foo": "bar",
-		},
-		Data: map[string]string{
-			"hello": "world",
-		},
-	}
-	bin, err := json.Marshal(req)
-	if err != nil {
-		panic(err)
-	}
-
-	for i := 0; i < b.N; i++ {
-		var data PacketRequest
-		err = json.Unmarshal(bin, &data)
-		if err != nil {
-			panic(err)
-		}
-	}
-}
-
-func BenchmarkMsgpackUnmarshal(b *testing.B) {
-	req := &PacketRequest{
-		Method: 12,
-		ID:     12345,
-		Context: map[string]string{
-			"foo": "bar",
-		},
-		Data: map[string]string{
-			"hello": "world",
-		},
-	}
-	bin, err := msgpack.Marshal(req)
-	if err != nil {
-		panic(err)
-	}
-
-	for i := 0; i < b.N; i++ {
-		var data PacketRequest
-		err = msgpack.Unmarshal(bin, &data)
-		if err != nil {
-			panic(err)
-		}
-	}
-}
-
-func BenchmarkTeaPackMarshal(b *testing.B) {
-	req := &PacketRequest{
-		Method: 12,
-		ID:     12345,
-		Context: map[string]string{
-			"foo": "bar",
-		},
-		Data: map[string]string{
-			"hello": "world",
-		},
-	}
-	for i := 0; i < b.N; i++ {
-		_, err := Marshal(req)
-		if err != nil {
-			panic(err)
-		}
-	}
-}
-
-func BenchmarkJSONMarshal(b *testing.B) {
-	req := &PacketRequest{
-		Method: 12,
-		ID:     12345,
-		Context: map[string]string{
-			"foo": "bar",
-		},
-		Data: map[string]string{
-			"hello": "world",
-		},
-	}
-	for i := 0; i < b.N; i++ {
-		_, err := json.Marshal(req)
-		if err != nil {
-			panic(err)
-		}
-	}
-}
-
-func BenchmarkMsgpackMarshal(b *testing.B) {
-	req := &PacketRequest{
-		Method: 12,
-		ID:     12345,
-		Context: map[string]string{
-			"foo": "bar",
-		},
-		Data: map[string]string{
-			"hello": "world",
-		},
-	}
-	for i := 0; i < b.N; i++ {
-		_, err := msgpack.Marshal(req)
-		if err != nil {
-			panic(err)
-		}
-	}
+	b, err := Marshal(req)
+	a.NoError(err)
+	p, err := Load(b)
+	a.NoError(err)
+	err = Unmarshal(p, &data)
+	a.NoError(err)
+	err = UnmarshalContext(p, &ctx)
+	a.NoError(err)
+	a.Equal("world", data["hello"])
+	a.Equal("bar", ctx["foo"])
+	a.Equal(StatusCodeOK, Status(req))
+	a.Equal(uint16(12345), ID(p))
 }
